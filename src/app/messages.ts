797679ea044cafb34fc69e27587b7bff1d931bd0
sreemathan <sreemathan.r@gmail.com>
2019-07-08 10:37:31 +0530

    1 file modified

--- src/app/messages.ts
+++ src/app/messages.ts
@@ -1,15 +1,14 @@
 export const messages = [
   {
-    text: 'Hello, how are you? This should be a very long message so that we can test how it fit into the screen.',
+    text: 'Meet Altran Assist',
     reply: false,
     date: new Date(),
     user: {
-      name: 'John Doe',
-      avatar: 'https://cresscap.com/wp-content/uploads/bfi_thumb/dummy-profile-pic-353fq072wibz1xp0b9j75s.png',
+      name: 'Altran Assist'
     },
   },
   {
-    text: 'Hello, how are you? This should be a very long message so that we can test how it fit into the screen.',
+    text: 'This should be a very long message so that we can test how it fit into the screen.',
     reply: true,
     date: new Date(),
     user: {
@@ -18,66 +17,11 @@ export const messages = [
     },
   },
   {
-    text: 'Hello, how are you?',
+    text: 'a conversational AI chatbot',
     reply: false,
     date: new Date(),
     user: {
-      name: 'John Doe',
-      avatar: '',
+      name: 'Altran Assist'
     },
-  },
-  {
-    text: 'Hey looks at that pic I just found!',
-    reply: false,
-    date: new Date(),
-    type: 'file',
-    files: [
-      {
-        url: 'https://cresscap.com/wp-content/uploads/bfi_thumb/dummy-profile-pic-353fq072wibz1xp0b9j75s.png',
-        type: 'image/jpeg',
-        icon: false,
-      },
-    ],
-    user: {
-      name: 'John Doe',
-      avatar: '',
-    },
-  },
-  {
-    text: 'What do you mean by that?',
-    reply: false,
-    date: new Date(),
-    type: 'quote',
-    quote: 'Hello, how are you? This should be a very long message so that we can test how it fit into the screen.',
-    user: {
-      name: 'John Doe',
-      avatar: '',
-    },
-  },
-  {
-    text: 'Attached is an archive I mentioned',
-    reply: true,
-    date: new Date(),
-    type: 'file',
-    files: [
-      {
-        url: 'https://cresscap.com/wp-content/uploads/bfi_thumb/dummy-profile-pic-353fq072wibz1xp0b9j75s.png',
-        icon: 'file-text-outline',
-      },
-    ],
-    user: {
-      name: 'John Doe',
-      avatar: '',
-    },
-  },
-  {
-    text: 'Meet me there',
-    reply: false,
-    date: new Date(),
-    type: 'text',
-    user: {
-      name: 'John Doe',
-      avatar: 'https://cresscap.com/wp-content/uploads/bfi_thumb/dummy-profile-pic-353fq072wibz1xp0b9j75s.png',
-    },
-  },
+  }
 ];
