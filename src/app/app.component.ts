be17de3ffc2a6f583fedc3dc6acd30f1ee32df9e
sreemathan <sreemathan.r@gmail.com>
2019-07-12 11:40:08 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, Renderer2, ElementRef, ViewChild} from '@angular/core';
 import { ChatShowcaseService } from './chat-showcase.service';
 
+
 declare var $: any;
 
 @Component({
@@ -18,7 +19,7 @@ export class AppComponent {
   
   constructor(protected chatShowcaseService: ChatShowcaseService, private renderer: Renderer2, private elem: ElementRef) {
     this.messages = this.chatShowcaseService.loadMessages();
-	console.log(this.messages);
+	console.log('onload',this.messages);
   }
   
   ngAfterViewInit() {
@@ -26,7 +27,7 @@ export class AppComponent {
 	  var chatButton = this.elem.nativeElement.querySelector('input.with-button');
 	  var chatBody = this.elem.nativeElement.querySelector('.messages');
 	  nbIcon.insertAdjacentHTML('beforeend', '<i class="fa fa-microphone" aria-hidden="true"></i>');
-	  chatBody.insertAdjacentHTML('beforeend', '<div class="chatloader"><img src="http://localhost:4200/assets/gif/loader.gif" class="align-center"></div>');
+	  chatBody.insertAdjacentHTML('beforeend', '<div class="chatloader"><img src="http://10.203.208.133:4200/assets/gif/loader.gif" class="align-center"></div>');
 	  
 	  this.renderer.listen(chatButton, 'keyup', (e) => {
 		  if(e.target.value == ""){
@@ -38,23 +39,63 @@ export class AppComponent {
 			nbIcon.classList.remove('animated', 'pulse','infinite');
 		  }
 	  });
-	  
-	}
+	  	  $( () => {
+    $( "#draggable" ).draggable();
+	$("body").on('click','a.ng-star-inserted', (event) => {
+		
+		var linkVal = event.currentTarget.href;
+		var ext = linkVal.substr( (linkVal.lastIndexOf('.') +1) );
+		console.log(ext);
+		if(ext == 'jpg' || ext == 'png'){
+			$("#openMedia").html('<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">Altran Assist</h5></div><div class="modal-body" style="text-align:center;"><img src="' + linkVal + '" style="width:40%"></div></div></div>');
+		}
+		if(ext == 'mp4'){
+			$("#openMedia").html('<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">Altran Assist</h5></div><div class="modal-body" style="text-align:center;"><video width="100%" height="450" controls><source src="' + linkVal + '" type="video/mp4"></video></div></div></div>');
+		}
+		if(ext == 'pdf'){
+			$("#openMedia").html('<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">Altran Assist</h5></div><div class="modal-body" style="text-align:center;"><iframe src="' + linkVal + '" width="100%" height="500px"></iframe></div></div></div>');
+		}
+		
+	});
+  } );
+	} 
 	
-
-    
   sendMessage(event: any) {
 	$('.chatloader').show();
 	const files = !event.files ? [] : event.files.map((file) => {
-      return {
-        url: file.src,
+      console.log('type',file.type);
+	  if(file.type == "image/jpeg") {
+	  return {
+        url: 'http://10.203.208.133:4200/assets/images/sampleimg.jpg',
         type: file.type,
         icon: 'file-text-outline',
       };
+	  }
+	  if(file.type == "image/png") {
+	  return {
+        url: 'https://cresscap.com/wp-content/uploads/bfi_thumb/dummy-profile-pic-353fq072wibz1xp0b9j75s.png',
+        type: file.type,
+        icon: 'file-text-outline',
+      };
+	  }
+      if(file.type == "video/mp4") {
+	  return {
+        url: 'http://10.203.208.133:4200/assets/images/sample.mp4',
+        type: file.type,
+        icon: 'file-text-outline',
+      };
+	  }
+      if(file.type == "application/pdf") {
+	  return {
+        url: 'http://10.203.208.133:4200/assets/images/sample.pdf',
+        type: file.type,
+        icon: 'file-text-outline',
+      };
+	  }
     });
-	
+	const msg = event.message.toLowerCase( );
     this.messages.push({
-      text: event.message,
+      text: msg,
       date: new Date(),
       reply: true,
       type: files.length ? 'file' : 'text',
@@ -65,7 +106,7 @@ export class AppComponent {
       },
     });
 	
-	if(event.message == "hello" || event.message == "hi" || event.message == "" ){
+	if(msg == "hello" || msg == "hi" || msg == "" ){
 	setTimeout(()=>{    
       $('.chatloader').hide();
 	  this.messages.push({
@@ -76,13 +117,63 @@ export class AppComponent {
 		  files: files,
 		  user: {
 			name: 'Altran Assist'
-		  },
+		  }
 		});
 	}, 2000);
 	
 		
+	} else if (msg == "what is this about" ||msg == "what" || msg == "about" ){
+	setTimeout(()=>{    
+      $('.chatloader').hide();
+	  this.messages.push({
+		  text: "This is a chatbot POC",
+		  date: new Date(),
+		  reply: false,
+		  type: 'text',
+		  files: files,
+		  user: {
+			name: 'Altran Assist'
+		  }
+		});
+	}, 2000);
+	
+		
+	} else if(msg == "image"){
+		setTimeout(()=>{    
+      $('.chatloader').hide();
+	  this.messages.push({
+		  text: "A sample image",
+		  date: new Date(),
+		  reply: false,
+		  type: 'file',
+		  files: [
+        {
+          url: 'http://10.203.208.133:4200/assets/images/sampleimg.jpg',
+          type: 'image/jpeg',
+        },
+      ],
+		  user: {
+			name: 'Altran Assist'
+		  }
+		});
+	}, 2000);
+	} else {
+		setTimeout(()=>{    
+      $('.chatloader').hide();
+	  this.messages.push({
+		  text: "Sorry couldn't recognize",
+		  date: new Date(),
+		  reply: false,
+		  type: 'text',
+		  files: files,
+		  user: {
+			name: 'Altran Assist'
+		  }
+		});
+	}, 2000);
 	}
-	console.log(this.messages);
+	
+	console.log('onload',this.messages);
     
   }
 }
